fe59680ea8503a74f9296b68cd384a720f19c733
rhee519 <rhee519@gmail.com>
2022-02-02 23:03:55 +0900

    fetch 2 years data

--- server.js
+++ server.js
@@ -14,15 +14,26 @@ const url_rest =
   "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo";
 
 const year = moment().year();
+const nextYear = year + 1;
 const data = {};
 let queryParams = `?${encodeURIComponent("serviceKey")}=${
   process.env.DATA_GO_KR_SERVICE_KEY
 }`; // Service key
 queryParams += `&${encodeURIComponent("_type")}=json`; // Data type
 queryParams += `&${encodeURIComponent("numOfRows")}=100`; // # of rows to fetch at once
-queryParams += `&${encodeURIComponent("solYear")}=${year}`; // Solar Year
+queryThisYear = `${queryParams}&${encodeURIComponent("solYear")}=${year}`; // This Year
+queryNextYear = `${queryParams}&${encodeURIComponent("solYear")}=${nextYear}`; // Next Year
 
-axios.get(url_rest + queryParams).then((res) => {
+axios.get(url_rest + queryThisYear).then((res) => {
+  const list = res.data.response.body.items.item;
+  list.forEach(({ dateName, locdate }, index) => {
+    if (dateName === "부처님오신날") dateName = "석가탄신일";
+    else if (dateName === "기독탄신일") dateName = "크리스마스";
+    data[locdate] = dateName;
+  });
+});
+
+axios.get(url_rest + queryNextYear).then((res) => {
   const list = res.data.response.body.items.item;
   list.forEach(({ dateName, locdate }, index) => {
     if (dateName === "부처님오신날") dateName = "석가탄신일";
