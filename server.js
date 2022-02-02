0b8b6015a3afb4dba62af08c39a0b2e13919e032
rhee519 <rhee519@gmail.com>
2022-02-02 20:52:43 +0900

    aim-server for fetching national holidays from data.go.kr

---
+++ server.js
@@ -0,0 +1,38 @@
+const { default: axios } = require("axios");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const moment = require("moment");
+const port = process.env.PORT || 3000;
+// const { getDoc, doc } = require("firebase/firestore");
+// const { signInWithEmailAndPassword } = require("firebase/auth");
+// const { auth, db } = require("./fbase");
+
+// 공공데이터포털로부터 국경일, 공휴일 fetch
+const url_rest =
+  "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo";
+
+const year = moment().year();
+const data = {};
+let queryParams = `?${encodeURIComponent("serviceKey")}=${
+  process.env.DATA_GO_KR_SERVICE_KEY
+}`; // Service key
+queryParams += `&${encodeURIComponent("_type")}=json`; // Data type
+queryParams += `&${encodeURIComponent("numOfRows")}=100`; // # of rows to fetch at once
+queryParams += `&${encodeURIComponent("solYear")}=${year}`; // Solar Year
+
+axios.get(url_rest + queryParams).then((res) => {
+  const list = res.data.response.body.items.item;
+  list.forEach(({ dateName, locdate }, index) => {
+    if (dateName === "부처님오신날") dateName = "석가탄신일";
+    else if (dateName === "기독탄신일") dateName = "크리스마스";
+    data[locdate] = dateName;
+  });
+});
+
+app.get("/", cors(), (req, res) => {
+  res.send(data);
+});
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
